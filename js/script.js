87b0a782d37d1866be9d66f750a1a9b359fb2a34
stuean <stue1409@stthomas.edu>
2019-04-20 16:36:57 -0500

    a

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 	//instantiate leaflet map
-	var map = L.map('map').setView([51.505, -0.09], 13);
-	var map2 = L.map('map2').setView([44.948, -93.106], 13);
+	
+	
+	var map = L.map('map').setView([48.86, 2.35], 13);
+	var map2 = L.map('map2').setView([52.52, 13.405], 13);
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
    	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map2);
+	
+	
+	
